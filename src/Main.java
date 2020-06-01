4a894cde38f51e92ad5abd83587d84c7427aa28b
MEGALUL <dimac2278@yandex.ru>
2020-06-01 17:59:50 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     private final static String NAME = "root";
     private final static String PASSWORD = "root";
     private final static String URL = "jdbc:mysql://localhost:3306/test?useSSL=false";
-    private final static int ELEMENTS = 30;
+    private final static int ITEMS = 30;
     private static final ConnectionManager connectionManager = new ConnectionManager();
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
@@ -18,7 +18,7 @@ public class Main {
         connectionManager.clearTable();
 
         Random r = new Random();
-        for (int i = 1; i < ELEMENTS; i++) {
+        for (int i = 1; i < ITEMS; i++) {
             String name = "items" + i;
             dataBaseManager.addItem(name, r.nextInt(1000 - 100) + 100);
         }
@@ -30,9 +30,9 @@ public class Main {
             String[] input = command.split(" ");
             try {
                 switch (input[0]) {
+                    case "/showall" -> dataBaseManager.showAll();
                     case "/add" -> dataBaseManager.addItem(input[1], Integer.parseInt(input[2]));
                     case "/delete" -> dataBaseManager.deleteItem(input[1]);
-                    case "/showall" -> dataBaseManager.showAll();
                     case "/showprice" -> dataBaseManager.showPrice(input[1]);
                     case "/changeprice" -> dataBaseManager.changePrice(input[1], Integer.parseInt(input[2]));
                     case "/filterbyprice" -> dataBaseManager.getPriceInterval(Integer.parseInt(input[1]), Integer.parseInt(input[2]));
